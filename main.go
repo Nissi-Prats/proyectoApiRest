3f15a8ef85664c3685074c0bc315b04dc52f6597
Nissi Prats <nissiprats.r6@gmail.com>
2026-05-20 21:37:51 -0600

    Cambiando el limite de la consulta

--- main.go
+++ main.go
@@ -88,6 +88,7 @@ func main() {
 			INNER JOIN titles t ON e.emp_no = t.emp_no
 			INNER JOIN salaries s ON e.emp_no = s.emp_no
 			WHERE t.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+			ORDER BY e.emp_no DESC
 			LIMIT 20;
 		`
 
